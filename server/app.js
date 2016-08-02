8824a926b9ebd2a2f87832e7df800134e93fea98
DARSHAN N <darshann12@gmail.com>
2016-08-02 19:06:05 +0530

    made the app to run, added a home or / route

--- server/app.js
+++ server/app.js
@@ -1,1 +1,16 @@
- 
+var express = require('express');
+var app = express();
+var routes = require('./routes');
+
+
+
+app.use(express.static(__dirname + '../client/assets'));
+app.use('/', routes);
+
+
+
+
+
+app.listen(3000, function() {
+  console.log('Listening on port 3000...')
+})
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,1 +1,12 @@
- 
+var express = require('express');
+var users = require('../routes/users');
+var router = express.Router();
+var path = require('path');
+router.use('/users', users);
+
+router.get('/', function(req, res){
+	res.sendFile(path.join(__dirname, '../../client/', 'index.html'));
+});
+
+
+module.exports = router;
