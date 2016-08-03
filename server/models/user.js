7beb47e1a561c0838e0160f2e2f9c36a1746f76a
Saurabh Mahambrey <saurabh_mahambrey@persistent.co.in>
2016-08-03 11:12:00 +0530

    user model structure added

--- server/models/user.js
+++ server/models/user.js
@@ -1,1 +1,20 @@
- 
+var dbObj = require('./../config/mongoconnection'); //mongoose object required to communicate with database
+
+var userSchema = {
+    fname: String,
+	lname: String,
+    username: String,
+    password: String,
+	telephone: String,
+	address: String,
+	email: String,
+   
+};
+
+
+var userSchemaObj = new dbObj.Schema(userSchema, {collection:"user", versionKey: false});  //creates our  schema structure
+
+var userObj       = dbObj.model("user", userSchemaObj);  //uses the schema created to form our model
+module.exports       = userObj;
+
+
