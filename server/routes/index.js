60e70b0fe51f54e5400df1dd4e8c2a1370923f79
DARSHAN N <darshann12@gmail.com>
2016-08-04 13:55:39 +0530

    created routes for all models

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 var express = require('express');
-var users = require('../routes/users');
 var router = express.Router();
 var path = require('path');
+var users = require('../routes/users');
+var advertisements = require('../routes/advertisements');
+var conversationHistory = require('../routes/conversationHistory');
+var transactions = require('../routes/transactions');
 
 
 router.use('/users', users);
-
-
+router.use('/advertisements', advertisements);
+router.use('/conversationHistory', conversationHistory);
+router.use('/transactions', transactions);
 
 router.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, '../../client/', 'index.html'));
