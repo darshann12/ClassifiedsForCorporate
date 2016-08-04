92febc1b907dfcccfee567e19ab8c0b48b7021a6
Saurabh Mahambrey <saurabh_mahambrey@persistent.co.in>
2016-08-04 11:58:10 +0530

    initial stage models added

--- server/models/ad.js
+++ server/models/ad.js
@@ -1,20 +1,29 @@
 var dbObj = require('./../config/mongoconnection'); //mongoose object required to communicate with database
 
 var adSchema = {
-    name:String,
+    name:{type:String, required : true},
+    saleType:{required : true , type: String, 
+          enum : ['rent','sell']},
+    saleType:{ type: String, 
+          enum : ['mobiles','laptops','home applicance','other']},
     dateCreated: { type: Date, default: Date.now },
+    dateExp:{type : Date, required : true},
+        
     images:[{
             url:String
             }
             ],
-    isOpenForSale:Boolean,
-    _creator : { type: Number, ref: 'User' }
-    
-   
-};
+    isOpenForSale:{type:Boolean,required : true },
+    creator : { type: String },
+    price : {type:Number ,required: true},
+    isNegotiable :{type:Boolean,required : true },
+    comments :[{ body: String, 
+                 date: Date.now,
+                 user : String}]
+  };
 
 
-var adSchemaObj = new dbObj.Schema(adSchema, {collection:"ad", versionKey: false});  //creates our  schema structure
+var adSchemaObj = new dbObj.Schema(adSchema, {collection:"advertisements", versionKey: false});  //creates our  schema structure
 
-var adObj       = dbObj.model("ad", adSchemaObj);  //uses the schema created to form our model
+var adObj       = dbObj.model("advertisements", adSchemaObj);  //uses the schema created to form our model
 module.exports       = adObj;
---
+++ server/models/transactions.js
@@ -0,0 +1,14 @@
+var dbObj = require('./../config/mongoconnection'); //mongoose object
+
+var transactionSchema = {
+seller : {type: String , required : true}, 
+buyer : {type : String, required : true},
+price : {type : Number, required : true},
+warrantyDate : {type : Date},
+date : { type: Date, default: Date.now }
+    }
+
+var transObj = new dbObj.Schema(transactionSchema,{collection:"transactionHistory",version : false}));
+
+var transactionObj = dbObj.Schema("transactionHistory",transObj);
+module.exports = transactionObj;
