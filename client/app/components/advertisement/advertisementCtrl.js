5c95dca2206075ce0f65b5ce4699a799fb70c1ec
shalvi kuba <shalvi_kubal@persistent.co.in>
2016-08-05 16:59:10 +0530

    changes to home.html and advertisement.html/js

--- client/app/components/advertisement/advertisementCtrl.js
+++ client/app/components/advertisement/advertisementCtrl.js
@@ -1,13 +1,14 @@
 var app=angular.module("cfc");
 
-app.controller("advertisementCtrl",['$scope','$state', '$stateParams'/*',advertisementService'*/,function($scope,$state,$stateParams/*,advertisementService*/){
+app.controller("advertisementCtrl",['$scope','$state', '$stateParams',',advertisementService',function($scope,$state,$stateParams,advertisementService){
     alert("inadvertisementCtrl:");
    
                     var category= $stateParams.category;
                     alert("value of category:"+category);
-     /*
+   
                     var option={"limit":6,
                                 "category":category
                     }
-                   $scope.advertisements= advertisementService.getAdvertisements(option);*/
+    
+                   $scope.advertisements= advertisementService.searchAdvertisements(option);
                 }]);
