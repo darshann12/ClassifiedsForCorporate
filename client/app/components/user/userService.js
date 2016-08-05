4542d5ad5c2264c84cba6dc044ad95ef9cdaa47c
DARSHAN N <darshann12@gmail.com>
2016-08-05 14:38:22 +0530

    created frontend angular services for all components

---
+++ client/app/components/user/userService.js
@@ -0,0 +1,52 @@
+var app.angular.module('cfc');
+app.factory('userService', ['$http', function($http) {
+                var factory = {};
+
+
+                factory.createUser = function(user) {
+                    $http.post("/users", user)
+                        .success(function(data, status, headers, config) {
+                            return response.data;
+                        })
+                        .error(function(data, status, header, config) {
+                            return "cannot fetch user";
+                        });
+
+                }
+
+                factory.getUser = function(userId) {
+                    $http.get("/users", {
+                            params: {
+                                username: userId
+                            }
+                        })
+                        .then(function(response) {
+                            return response.data;
+                        });
+
+                }
+
+
+                factory.updateUser = function(user) {
+                    $http.put("/users", user)
+                        .success(function(data, status, headers, config) {
+                            return response.data;
+                        })
+                        .error(function(data, status, header, config) {
+                            return "cannot update user";
+                        });
+                }
+                    factory.deleteUser = function(user) {
+                        $http.delete("/users", user)
+                            .success(function(data, status, headers, config) {
+                                return response.data;
+                            })
+                            .error(function(data, status, header, config) {
+                                return "cannot fetch user";
+                            });
+
+
+                        return factory;
+                    }
+                    
+}]);
