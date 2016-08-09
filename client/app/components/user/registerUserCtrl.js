199bee471c17a64cda3638601e1f5a062475a4b3
DARSHAN N <darshann12@gmail.com>
2016-08-09 18:45:57 +0530

    fixed UI bug in registeruserView

--- client/app/components/user/registerUserCtrl.js
+++ client/app/components/user/registerUserCtrl.js
@@ -1,6 +1,6 @@
 var app=angular.module("cfc");
 
-app.controller("registerUserCtrl",['$scope','userService',function($scope,userService){
+app.controller("registerUserCtrl",['$scope','userService','$state',function($scope,userService,$state){
     $scope.user={};
     $scope.createUser=function(){
     userService.createUser($scope.user).then(function(response){
@@ -8,7 +8,15 @@ app.controller("registerUserCtrl",['$scope','userService',function($scope,userSe
 
         if(response.data.username===$scope.user.username){
             alert("your account has been created successfully");
+            $state.go("login");
         }
+        else{
+        
+            alert("failed to create account");
+            
+        }
+        
+        
     })
     
     }
