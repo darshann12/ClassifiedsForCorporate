16d2b6d7ecb21d242590351d7178a00b2eea37b3
DARSHAN N <darshann12@gmail.com>
2016-08-23 18:54:22 +0530

    chat window UI improved

--- client/app/components/conversationHistory/myMessagesCtrl.js
+++ client/app/components/conversationHistory/myMessagesCtrl.js
@@ -5,7 +5,8 @@ app.controller('myMessagesCtrl',['$scope','chatSocket','$rootScope','$stateParam
 
     $scope.toUser=$stateParams.toUser;
     chatSocket.on('newMessage',function(data){
-
+        $scope.toUser=data.from;
+        data.css="pull-left text-muted"
         $scope.messageThread.push(data); 
         $scope.message=null;
     })   
@@ -16,7 +17,7 @@ app.controller('myMessagesCtrl',['$scope','chatSocket','$rootScope','$stateParam
     })    
     $scope.sendMessage=function(){
 
-        $scope.messageThread.push({from:"You",to:$scope.toUser,message:$scope.message})
+        $scope.messageThread.push({from:"You",to:$scope.toUser,message:$scope.message,css:"pull-right text-muted"})
         chatSocket.emit('sendMessage',{from:$rootScope.username,to:$scope.toUser,message:$scope.message});
 
     }
